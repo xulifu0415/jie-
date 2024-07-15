481c491a9f116796b91f08ac835a41000b912861
action <action@github.com>
2024-07-15 07:22:46 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Mon Jul 15 2024 07:22:01 GMT+0000 (Coordinated Universal Time)
+//Mon Jul 15 2024 07:22:46 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const jsname = "屈臣氏",
