e9f85406a6b5cf5608ba90d1c6fabdc75ee8ba55
action <action@github.com>
2024-07-20 14:12:53 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sat Jul 20 2024 03:46:26 GMT+0000 (Coordinated Universal Time)
+//Sat Jul 20 2024 14:12:53 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("399乐园");
