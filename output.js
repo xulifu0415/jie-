cbadc2302c35ceed955e85e05a8b2bbf0674d950
action <action@github.com>
2024-07-21 09:05:04 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sun Jul 21 2024 09:04:33 GMT+0000 (Coordinated Universal Time)
+//Sun Jul 21 2024 09:05:04 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("番茄小说");
